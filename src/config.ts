5fa4bb21c676dee7c4c43cfa1fcd40746afdece4
tonisives <tonisives@gmail.com>
2024-08-25 17:12:11 +0300

    update

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb"
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
 
-export const dbClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+export const dbClient = DynamoDBDocumentClient.from(new DynamoDBClient({}), {
+  marshallOptions: { removeUndefinedValues: true, convertEmptyValues: true },
+})
